4e3fea5b7aafc89ca7d2ac27ff7a525586ce1fee
Tuttep Tanwattanakul <KillerzScrubbie@users.noreply.github.com>
2021-01-02 13:11:29 +0700

    SpawnPotion Class

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -7,31 +7,14 @@ public class SpawnerSample : MonoBehaviour
 
     void Start()
     {
-        int angle = 15;
         int radius = 5;
+    }
 
-        Vector3 spawnPosition = transform.position;
-
+    void SpawnPotion(int angle)
+    {
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 55;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 95;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 135;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
         spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
-
-    
 }
 
